3a5798e3666f855a6c8e0eecf7280a8571841d59
plinioteixeira <plinio.t.vasconcellos@gmail.com>
2022-05-24 17:00:33 -0300

    Atualizar o fabricante do produto

--- includes/funcoes-produtos.php
+++ includes/funcoes-produtos.php
@@ -30,7 +30,7 @@ function inserirProduto($conexao, $nome, $preco, $quantidade, $descricao, $fabri
 
 function lerUmProduto($conexao, $id){
     // Montagem do comando SQL com o parâmetro id
-    $sql = "SELECT id, nome FROM fabricantes WHERE id = $id";
+    $sql = "SELECT id, nome, preco, quantidade, descricao, fabricante_id FROM produtos WHERE id = $id";
 
     // Execução do comando e armazenamento do resultado 
     $resultado = mysqli_query($conexao, $sql)
