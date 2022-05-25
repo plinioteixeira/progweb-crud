4277341d912f3ad2a701919395e6088577288504
plinioteixeira <plinio.t.vasconcellos@gmail.com>
2022-05-25 16:43:26 -0300

    Aplicando CSS do Bootstrap

--- fabricantes/listar.php
+++ fabricantes/listar.php
@@ -8,9 +8,37 @@ $listaDeFabricantes = lerFabricantes($conexao);
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title> Fabricantes | SELECT - CRUD com PHP e MySQL </title>
-<link href="../css/style.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+<style>
+    .btn-primary{
+        background-color: darkblue;
+    }
+</style>
 </head>
 <body>
+<nav  class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+Recursos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Preços</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Desativado</a>
+      </li>
+    </ul>
+  </div>
+</nav>
 
 <div class="container">
     <h1>Fabricantes | SELECT -
@@ -21,11 +49,11 @@ $listaDeFabricantes = lerFabricantes($conexao);
 <div class="container">
     
     <h2>Lendo e carregando todos os fabricantes</h2>
-    <p><a href="inserir.php">Inserir</a></p>    
+    <p><a class="btn btn-primary btn-lg" href="inserir.php">Inserir</a></p>    
 
-    <table>
+    <table class="table table-striped table-dark">
         <caption> Lista de Fabricantes </caption>
-        <thead>
+        <thead class="thead-dark">
             <tr>
                 <th>ID</th>
                 <th>Nome</th>
@@ -42,8 +70,8 @@ foreach ($listaDeFabricantes as $fabricante) { ?>
             <td><?=$fabricante["id"]?></td>
             <td><?=$contador. ":"  .$fabricante["nome"]?></td>
             <td>
-                <a href="atualizar.php?id=<?=$fabricante["id"]?>">Atualizar</a>
-                 - <a href="excluir.php?id=<?=$fabricante["id"]?>">Excluir</a>
+                <a class="btn btn-warning" href="atualizar.php?id=<?=$fabricante["id"]?>"><span class="alert-link">Atualizar</span></a>
+                 - <a class="btn btn-danger" href="excluir.php?id=<?=$fabricante["id"]?>"><span class="alert-link">Excluir</span></a>
             </td>
         </tr>
 <?php
@@ -58,6 +86,9 @@ $contador++;
  
 </div>
 
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 
 
 </body>
